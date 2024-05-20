1839cd1b852fc762522ec360901012cba83367a0
p.karari <kararikelvin2002@gmail.com>
2024-05-20 13:29:19 +0300

    Adds delete functionality to the project

--- script.js
+++ script.js
@@ -1,39 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let new_btn = document.getElementsByClassName('new_btn');
+    const button = document.querySelector('.new_btn');
     const new_task = document.getElementsByClassName('new_task');
     let task_value = new_task.value;
-    const para = document.querySelectorAll('p')
-    const del = document.getElementsByClassName('btn');
-    function del_function() {
-        para.style.textDecoration = "line-through"
-    }
-    del.addEventListener('click', function() {
-        del_function();
-    })
+    const para = document.querySelectorAll('p');
+    const del = document.querySelectorAll('.btn');
+
+    
+    
+    del.forEach((button, index) => {
+        button.addEventListener('click', function() {
+            para[index].style.textDecoration = "line-through"
+            para[index].style.color = "gray"
+        });
+    });
 
 
-    // function add_task() {
+    function add_task() {
         
-    //     const task = document.createElement('div')
-    //     const btn = document.createElement('button');
-    //     const p = document.createElement('p');
+        const task = document.createElement('div')
+        const btn = document.createElement('button');
+        const p = document.createElement('p');
 
-    //     btn.textContent = '';
-    //     p.textContent = task_value;
+        btn.textContent = '';
+        p.textContent = task_value;
             
 
-    //     task.classList.add('task');
-    //     btn.classList.add('btn');
+        task.classList.add('task');
+        btn.classList.add('btn');
 
-    //     task.appendChild(btn);
-    //     task.appendChild(p);
+        task.appendChild(btn);
+        task.appendChild(p);
 
-    //     new_task.value = '';
+        new_task.value = '';
 
-    //     }
-    // console.log(new_btn)
-    // new_btn.addEventListener('click', function() {
-    //     add_task()
+    }
+    // console.log(new_button)
+    
+
+    button.addEventListener('click', function() {
+        new_task.textContent = ' ';
+    });
+    // new_btn[0].addEventListener('click', function() {
+    //     console.log("clicked")
     // })
+    
+    
 })
 
